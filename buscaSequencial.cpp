02c4e1b3cfdf253cfb7fb63c608a47de3dce29ce
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2024-08-08 21:36:10 -0300

    Add files via upload

---
+++ buscaSequencial.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+using namespace std::chrono;
+
+long buscaSequencial(vector<long>& v, long alvo){
+  for(long a : v) {
+      if(a==alvo) {
+          return a;
+      }
+  }
+  return -1;
+}
+
+int main() {
+    long nExecs = 100000;//10^5
+   const long double conversor = 1e+9;
+   vector<bool> resultado(nExecs + 1);
+
+   // Inicializando o gerador de números aleatórios
+   random_device rd;
+   mt19937 gen(rd());
+   uniform_int_distribution<> dis(0, 10000000);
+    for(int w=0; w<4; w++){
+        long tamVetor = 10000 * pow(10, w); // Aumenta o tamanho do vetor por um fator de 10 em cada iteração
+        cout << tamVetor << endl;
+        
+        vector<long> vetor(tamVetor);
+            generate(vetor.begin(), vetor.end(), [&]() { return dis(gen); });
+  
+            auto tempoInicio = high_resolution_clock::now();
+
+            for(long i=0; i<=nExecs; i++) {
+                long alvo = vetor[dis(gen) % vetor.size()];
+                resultado[i] = buscaSequencial(vetor, alvo);
+            }
+            auto tempoFim = high_resolution_clock::now();
+            long double tempo = duration_cast<nanoseconds>(tempoFim-tempoInicio).count();
+            cout << "tempo final: " << tempo/conversor << " s" << endl; 
+            
+    }
+
+  return 0;
+}
+
+
+
+
+
